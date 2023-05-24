b6b9a158ed62dc0121a6d67cb2fad67799f6c040
andistraum <andreas.armanious@edu.fh-joanneum.at>
2023-05-24 11:56:45 +0200

    added current db schema with bulk data inserted

--- create-db.sql
+++ create-db.sql
@@ -1,16 +1,16 @@
 CREATE
-DATABASE ischedule;
+    DATABASE ischedule;
 
 USE
-ischedule;
+    ischedule;
 
 CREATE TABLE users
 (
-    id       INT          NOT NULL AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL,
-    email    VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role     ENUM('STUDENT', 'ADMIN', 'ASSISTANT') NOT NULL,
+    id       INT                                    NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255)                           NOT NULL,
+    email    VARCHAR(255)                           NOT NULL,
+    password VARCHAR(255)                           NOT NULL,
+    role     ENUM ('STUDENT', 'ADMIN', 'ASSISTANT') NOT NULL,
     PRIMARY KEY (id),
     UNIQUE KEY (email)
 );
@@ -42,12 +42,12 @@ CREATE TABLE schedules
 CREATE TABLE course_schedule
 (
     id          INT PRIMARY KEY AUTO_INCREMENT,
-    course_id   INT  NOT NULL,
-    schedule_id INT  NOT NULL,
-    day_of_week ENUM('MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN') NOT NULL,
-    start_time  TIME NOT NULL,
-    end_time    TIME NOT NULL,
-    room_id     INT  NOT NULL,
+    course_id   INT                                                    NOT NULL,
+    schedule_id INT                                                    NOT NULL,
+    day_of_week ENUM ('MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN') NOT NULL,
+    start_time  TIME                                                   NOT NULL,
+    end_time    TIME                                                   NOT NULL,
+    room_id     INT                                                    NOT NULL,
     CONSTRAINT unique_course_schedule UNIQUE (course_id, schedule_id, day_of_week, start_time),
     FOREIGN KEY (course_id) REFERENCES courses (id),
     FOREIGN KEY (schedule_id) REFERENCES schedules (id),
@@ -61,44 +61,53 @@ CREATE TABLE preferences
     preferred_start_time TIME NOT NULL,
     preferred_end_time   TIME NOT NULL,
     preferred_room_id    INT,
-    prefered_day_of_week ENUM('MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'),
+    prefered_day_of_week ENUM ('MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'),
     CONSTRAINT fk_user_id_for_preferences FOREIGN KEY (user_id) REFERENCES users (id),
     CONSTRAINT fk_preferred_room_id FOREIGN KEY (preferred_room_id) REFERENCES rooms (id)
-)
-    -- create bulk data --
--- Insert sample courses
-    INSERT INTO courses (description, name, admin_id, instructor_id) VALUES
-('Course 1 description', 'Course 1', 1, 1),
-('Course 2 description', 'Course 2', 1, 2),
-('Course 3 description', 'Course 3', 1, 1);
+);
 
--- Insert sample rooms
-INSERT INTO rooms (capacity, name)
-VALUES (30, 'Room 101'),
-       (25, 'Room 102'),
-       (20, 'Room 103');
 
--- Insert sample schedules
-INSERT INTO schedules (week_number, user_id)
-VALUES (1, 1),
-       (2, 1),
-       (1, 6);
+-- create bulk data --
 
--- Insert sample course schedules
-INSERT INTO course_schedule (day_of_week, end_time, start_time, course_id, room_id, schedule_id)
-VALUES ('Monday', '10:00:00', '09:00:00', 1, 1, 1),
-       ('Wednesday', '12:00:00', '11:00:00', 1, 1, 2),
-       ('Friday', '14:00:00', '13:00:00', 2, 2, 1),
-       ('Tuesday', '16:00:00', '15:00:00', 3, 3, 3);
 
--- Insert sample user-course enrollments
-INSERT INTO user_course (user_id, course_id, id)
+-- Insert sample data into the 'users' table
+INSERT INTO `users` (`id`, `email`, `password`, `role`, `username`)
+VALUES (1, 'john@example.com', '$2y$12$qZBG.TiVcPJrWW7wZL2zOu988sfqASQ2BAfk2mEkb1FkwnzJSzm2W', 'STUDENT', 'john'),
+       (2, 'emma@example.com', '$2y$12$qZBG.TiVcPJrWW7wZL2zOu988sfqASQ2BAfk2mEkb1FkwnzJSzm2W', 'STUDENT', 'emma'),
+       (3, 'admin@example.com', '$2y$12$qZBG.TiVcPJrWW7wZL2zOu988sfqASQ2BAfk2mEkb1FkwnzJSzm2W', 'ADMIN', 'admin');
+
+-- Insert sample data into the 'courses' table
+INSERT INTO `courses` (`id`, `description`, `name`, `admin_id`, `instructor_id`)
+VALUES (1, 'Introduction to Programming', 'Programming 101', 3, 1),
+       (2, 'Web Development Basics', 'Web Dev 101', 3, 2),
+       (3, 'Database Management', 'DBMS', 3, 1);
+
+-- Insert sample data into the 'rooms' table
+INSERT INTO `rooms` (`id`, `capacity`, `name`)
+VALUES (1, 30, 'Room A'),
+       (2, 25, 'Room B'),
+       (3, 40, 'Room C');
+
+-- Insert sample data into the 'schedules' table
+INSERT INTO `schedules` (`id`, `week_number`, `user_id`)
 VALUES (1, 1, 1),
-       (1, 2, 2),
-       (6, 1, 3),
-       (7, 3, 4);
+       (2, 2, 2),
+       (3, 3, 1);
 
--- Insert sample preferences
-INSERT INTO preferences (prefered_day_of_week, preferred_end_time, preferred_start_time, preferred_room_id, user_id)
-VALUES ('Monday', '12:00:00', '10:00:00', 1, 1),
-       ('Friday', '16:00:00', '14:00:00', 2, 1);
+-- Insert sample data into the 'course_schedule' table
+INSERT INTO `course_schedule` (`id`, `day_of_week`, `end_time`, `start_time`, `course_id`, `room_id`, `schedule_id`)
+VALUES (1, 'Monday', '12:00:00', '10:00:00', 1, 1, 1),
+       (2, 'Wednesday', '14:00:00', '12:00:00', 2, 2, 2),
+       (3, 'Friday', '16:00:00', '14:00:00', 3, 3, 3);
+
+-- Insert sample data into the 'preferences' table
+INSERT INTO `preferences` (`id`, `preferred_day_of_week`, `preferred_end_time`, `preferred_start_time`,
+                           `preferred_room_id`, `user_id`)
+VALUES (1, 'Monday', '18:00:00', '16:00:00', 1, 1),
+       (2, 'Tuesday', '20:00:00', '18:00:00', 2, 2);
+
+-- Insert sample data into the 'user_course' table
+INSERT INTO `user_course` (`user_id`, `course_id`)
+VALUES (1, 1),
+       (2, 2),
+       (1, 3);
---
+++ ischedule.sql
@@ -0,0 +1,329 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 24. Mai 2023 um 11:53
+-- Server-Version: 10.4.27-MariaDB
+-- PHP-Version: 8.1.12
+
+SET
+SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET
+time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `ischedule`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `courses`
+--
+
+CREATE TABLE `courses`
+(
+    `id`            int(11) NOT NULL,
+    `description`   varchar(1000) DEFAULT NULL,
+    `name`          varchar(255) NOT NULL,
+    `admin_id`      int(11) DEFAULT NULL,
+    `instructor_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `courses`
+--
+
+INSERT INTO `courses` (`id`, `description`, `name`, `admin_id`, `instructor_id`)
+VALUES (1, 'Introduction to Programming', 'Programming 101', 3, 1),
+       (2, 'Web Development Basics', 'Web Dev 101', 3, 2),
+       (3, 'Database Management', 'DBMS', 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `course_schedule`
+--
+
+CREATE TABLE `course_schedule`
+(
+    `id`          int(11) NOT NULL,
+    `day_of_week` varchar(255) NOT NULL,
+    `end_time`    time         NOT NULL,
+    `start_time`  time         NOT NULL,
+    `course_id`   int(11) DEFAULT NULL,
+    `room_id`     int(11) DEFAULT NULL,
+    `schedule_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `course_schedule`
+--
+
+INSERT INTO `course_schedule` (`id`, `day_of_week`, `end_time`, `start_time`, `course_id`, `room_id`, `schedule_id`)
+VALUES (1, 'Monday', '12:00:00', '10:00:00', 1, 1, 1),
+       (2, 'Wednesday', '14:00:00', '12:00:00', 2, 2, 2),
+       (3, 'Friday', '16:00:00', '14:00:00', 3, 3, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `preferences`
+--
+
+CREATE TABLE `preferences`
+(
+    `id`                    int(11) NOT NULL,
+    `preferred_day_of_week` varchar(255) DEFAULT NULL,
+    `preferred_end_time`    time         DEFAULT NULL,
+    `preferred_start_time`  time         DEFAULT NULL,
+    `preferred_room_id`     int(11) DEFAULT NULL,
+    `user_id`               int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `preferences`
+--
+
+INSERT INTO `preferences` (`id`, `preferred_day_of_week`, `preferred_end_time`, `preferred_start_time`,
+                           `preferred_room_id`, `user_id`)
+VALUES (1, 'Monday', '18:00:00', '16:00:00', 1, 1),
+       (2, 'Tuesday', '20:00:00', '18:00:00', 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `rooms`
+--
+
+CREATE TABLE `rooms`
+(
+    `id`       int(11) NOT NULL,
+    `capacity` int(11) DEFAULT NULL,
+    `name`     varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `rooms`
+--
+
+INSERT INTO `rooms` (`id`, `capacity`, `name`)
+VALUES (1, 30, 'Room A'),
+       (2, 25, 'Room B'),
+       (3, 40, 'Room C');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `schedules`
+--
+
+CREATE TABLE `schedules`
+(
+    `id`          int(11) NOT NULL,
+    `week_number` int(11) DEFAULT NULL,
+    `user_id`     int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `schedules`
+--
+
+INSERT INTO `schedules` (`id`, `week_number`, `user_id`)
+VALUES (1, 1, 1),
+       (2, 2, 2),
+       (3, 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `users`
+--
+
+CREATE TABLE `users`
+(
+    `id`       int(11) NOT NULL,
+    `email`    varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `role`     varchar(255) NOT NULL,
+    `username` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `role`, `username`)
+VALUES (1, 'john@example.com', '$2y$12$qZBG.TiVcPJrWW7wZL2zOu988sfqASQ2BAfk2mEkb1FkwnzJSzm2W', 'STUDENT', 'john'),
+       (2, 'emma@example.com', '$2y$12$qZBG.TiVcPJrWW7wZL2zOu988sfqASQ2BAfk2mEkb1FkwnzJSzm2W', 'STUDENT', 'emma'),
+       (3, 'admin@example.com', '$2y$12$qZBG.TiVcPJrWW7wZL2zOu988sfqASQ2BAfk2mEkb1FkwnzJSzm2W', 'ADMIN', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user_course`
+--
+
+CREATE TABLE `user_course`
+(
+    `user_id`   int(11) NOT NULL,
+    `course_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `user_course`
+--
+
+INSERT INTO `user_course` (`user_id`, `course_id`)
+VALUES (1, 1),
+       (1, 3),
+       (2, 2);
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `courses`
+--
+ALTER TABLE `courses`
+    ADD PRIMARY KEY (`id`),
+  ADD KEY `FKp8k8u77nf521mtkpaumuy4nho` (`admin_id`),
+  ADD KEY `FKcyfum8goa6q5u13uog0563gyp` (`instructor_id`);
+
+--
+-- Indizes für die Tabelle `course_schedule`
+--
+ALTER TABLE `course_schedule`
+    ADD PRIMARY KEY (`id`),
+  ADD KEY `FKahfhilt2apyqcx619gvvc1pnw` (`course_id`),
+  ADD KEY `FKk9cmt5jjq4bhcn19c26uwlty8` (`room_id`),
+  ADD KEY `FKl9ufcldole32nbwqt9ono8ml7` (`schedule_id`);
+
+--
+-- Indizes für die Tabelle `preferences`
+--
+ALTER TABLE `preferences`
+    ADD PRIMARY KEY (`id`),
+  ADD KEY `FKjosif7ab7dfsp13qkm0vs1w6o` (`preferred_room_id`),
+  ADD KEY `FKme1hmam0h8w07410h7qkjna5m` (`user_id`);
+
+--
+-- Indizes für die Tabelle `rooms`
+--
+ALTER TABLE `rooms`
+    ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `schedules`
+--
+ALTER TABLE `schedules`
+    ADD PRIMARY KEY (`id`),
+  ADD KEY `FKd4y4xekwahv9boo6lc8gfl3jv` (`user_id`);
+
+--
+-- Indizes für die Tabelle `users`
+--
+ALTER TABLE `users`
+    ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UK_6dotkott2kjsp8vw4d0m25fb7` (`email`),
+  ADD UNIQUE KEY `UK_r43af9ap4edm43mmtq01oddj6` (`username`);
+
+--
+-- Indizes für die Tabelle `user_course`
+--
+ALTER TABLE `user_course`
+    ADD PRIMARY KEY (`user_id`, `course_id`),
+  ADD KEY `FKf4qni5wnlq0x70fm39w9tv7x4` (`course_id`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `courses`
+--
+ALTER TABLE `courses`
+    MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT für Tabelle `course_schedule`
+--
+ALTER TABLE `course_schedule`
+    MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT für Tabelle `preferences`
+--
+ALTER TABLE `preferences`
+    MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT für Tabelle `rooms`
+--
+ALTER TABLE `rooms`
+    MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT für Tabelle `schedules`
+--
+ALTER TABLE `schedules`
+    MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT für Tabelle `users`
+--
+ALTER TABLE `users`
+    MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `courses`
+--
+ALTER TABLE `courses`
+    ADD CONSTRAINT `FKcyfum8goa6q5u13uog0563gyp` FOREIGN KEY (`instructor_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `FKp8k8u77nf521mtkpaumuy4nho` FOREIGN KEY (`admin_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints der Tabelle `course_schedule`
+--
+ALTER TABLE `course_schedule`
+    ADD CONSTRAINT `FKahfhilt2apyqcx619gvvc1pnw` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`),
+  ADD CONSTRAINT `FKk9cmt5jjq4bhcn19c26uwlty8` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`),
+  ADD CONSTRAINT `FKl9ufcldole32nbwqt9ono8ml7` FOREIGN KEY (`schedule_id`) REFERENCES `schedules` (`id`);
+
+--
+-- Constraints der Tabelle `preferences`
+--
+ALTER TABLE `preferences`
+    ADD CONSTRAINT `FKjosif7ab7dfsp13qkm0vs1w6o` FOREIGN KEY (`preferred_room_id`) REFERENCES `rooms` (`id`),
+  ADD CONSTRAINT `FKme1hmam0h8w07410h7qkjna5m` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints der Tabelle `schedules`
+--
+ALTER TABLE `schedules`
+    ADD CONSTRAINT `FKd4y4xekwahv9boo6lc8gfl3jv` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints der Tabelle `user_course`
+--
+ALTER TABLE `user_course`
+    ADD CONSTRAINT `FKf4qni5wnlq0x70fm39w9tv7x4` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`),
+  ADD CONSTRAINT `FKoc4yl478i6o8hf240vumhjgrb` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
